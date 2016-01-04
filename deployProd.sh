f038c3fce58bece505d78632c4298ab961c3aae5
Till Gartner <till.gartner@gmail.com>
2016-01-04 10:26:40 +0700

    New post about where you've been

--- deployProd.sh
+++ deployProd.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-set -o errexit -o nounset
-
-rev=$(git rev-parse --short HEAD)
-
-cd output
-
-git init
-git config user.name "Till Gartner"
-git config user.email "till.gartner@gmail.com"
-
-git remote add upstream "https://$GH_TOKEN@github.com/tillg/tillgartner.com"
-git fetch upstream
-git reset upstream/gh-pages
-
-echo "tillgartner.com" > CNAME
-
-touch .
-
-git add -A .
-git commit -m "rebuild pages at ${rev}"
-git push -q upstream HEAD:gh-pages
+git add .
+git commit -m $1
+git push
